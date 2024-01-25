c795f78cba8d1c983b4f1d8f576188d2de6d574e
GenSayer <64618080+GenSayer@users.noreply.github.com>
2024-01-25 15:56:13 -0800

    Make the background more alligned

--- Background.js
+++ Background.js
@@ -1,12 +1,18 @@
 class Background {
-    constructor(game, x, y) {
+    constructor(game, x, y, height, width) {
         Object.assign(this, { game, x, y });
         this.spritesheet = ASSET_MANAGER.getAsset("./maps/map1.png");
         this.zoomLevel = 4;
+        this.x = 0;
+        this.y = 0;
+        this.height = 800;
+        this.width = 2500;
+        this.speed = 20;
     }
 
     update() {
         // You can add update logic specific to the background if needed
+        this.x -= this.speed;
     }
 
     draw(ctx) {
@@ -21,6 +27,7 @@ class Background {
    ///     const destinationHeight = this.spritesheet.height * this.zoomLevel;
 ///
         ctx.drawImage(this.spritesheet,
-            0,0);
+            0, 0, this.width, this.height);
+        ctx.drawImage(this.spritesheet, 0 + this.width, 0, this.width, this.height);
     }
 }
