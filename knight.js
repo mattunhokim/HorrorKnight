801c590651eb2a550b5274999cb5640c1e063464
GenSayer <64618080+GenSayer@users.noreply.github.com>
2024-01-25 18:04:34 -0800

    The knight has arrived but he isn't showing on the canvas yet

---
+++ knight.js
@@ -0,0 +1,22 @@
+class knight {
+
+    constructor(game) {
+        this.game = game;
+        this.animator = new animator(ASSET_MANAGER.getAsset("./movement1.png"), 46, 0, 21, 44, 8, 1.25);
+
+        this.x = 0;
+        this.y = 0;
+        this.speed = 20;
+    };
+
+    update() {
+        this.x += this.speed * this.game.clockTick;
+        if (this.x > 1024) this.x = 0;
+    };
+
+    draw(ctx) {
+        //ctx.drawImage(ASSET_MANAGER.getAsset("./movement1.png"),0,0);
+        this.animator.drawFrame(this.game.clockTick, ctx, this.x, this.y);
+    };
+
+}
