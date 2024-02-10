42de39b034996bf43b0d3f7309e073ef389203ba
GenSayer <64618080+GenSayer@users.noreply.github.com>
2024-02-10 13:55:58 -0800

    Add another enemy

--- enemies.js
+++ enemies.js
@@ -35,5 +35,40 @@ class Dragon {
 
 }
 
+class catfighter {
+    constructor(game, x, y) {
+        Object.assign(this, { game, x, y });
+        this.velocity = { x: -PARAMS.BITWIDTH, y: PARAMS.BITWIDTH * 3 };
+
+        this.spritesheet = ASSET_MANAGER.getAsset("./assets/catfighter.png");
+        this.animation = new Animator(this.spritesheet, 0, 0, 64, 64, 1, 0.5, 0, false, true);
+        this.paused = true;
+        this.dead = false;
+        this.deadCounter = 0;
+        this.flickerFlag = true;
+        this.updateBB();
+    }
+
+    updateBB() {
+        this.BB = new BoundingBox(this.x, this.y, PARAMS.BLOCKWIDTH, PARAMS.BLOCKWIDTH);
+    };
+
+    update() {
+        if (this.dead) {
+            this.deadCounter++;
+            if (this.deadCounter > 50) {
+                this.removeFromWorld = true;
+            }
+        } else {
+            this.x += this.velocity.x;
+            this.y += this.velocity.y;
+            this.updateBB();
+        }
+    };
+
+    draw(ctx) {
+        this.animations.drawFrame(this.game.clockTick, ctx, this.x - this.game.camera.x, this.y, PARAMS.SCALE);
+    };
+}
 
 
