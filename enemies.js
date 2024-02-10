13689dd4ebae3f867896781a604622b4bcea09d1
GenSayer <64618080+GenSayer@users.noreply.github.com>
2024-02-09 17:26:48 -0800

    Added an enemy and removed the wall class (not added in the game yet)

---
+++ enemies.js
@@ -0,0 +1,39 @@
+class Dragon {
+    constructor(game, x, y) {
+        Object.assign(this, { game, x, y });
+        this.velocity = { x: -PARAMS.BITWIDTH, y: PARAMS.BITWIDTH * 3 };
+
+        this.spritesheet = ASSET_MANAGER.getAsset("./assets/dragon.png");
+        this.animation = new Animator(this.spritesheet, 0, 160, 100, 100, 1, 0.5, 0, false, true);
+        this.paused = true;
+        this.dead = false;
+        this.deadCounter = 0;
+        this.flickerFlag = true;
+        this.updateBB();
+    };
+
+    updateBB() {
+        this.BB = new BoundingBox(this.x, this.y, PARAMS.BLOCKWIDTH, PARAMS.BLOCKWIDTH);
+    };
+
+    update() {
+        if (this.dead) {
+            this.deadCounter++;
+            if (this.deadCounter > 50) {
+                this.removeFromWorld = true;
+            }
+        } else {
+            this.x += this.velocity.x;
+            this.y += this.velocity.y;
+            this.updateBB();
+        }
+    };
+
+    draw(ctx) {
+        this.animations.drawFrame(this.game.clockTick, ctx, this.x - this.game.camera.x, this.y, PARAMS.SCALE);
+    };
+
+}
+
+
+
